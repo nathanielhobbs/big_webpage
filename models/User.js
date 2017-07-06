65332af10f15107e0fa0aaaf896771064820a4d8
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2017-07-06 10:32:58 +0800

    added new fields to User account to represent BJI players

--- models/User.js
+++ models/User.js
@@ -8,9 +8,14 @@ var Types = keystone.Field.Types;
 var User = new keystone.List('User');
 
 User.add({
-	name: { type: Types.Name, required: true, index: true },
-	email: { type: Types.Email, initial: true, required: true, unique: true, index: true },
-	password: { type: Types.Password, initial: true, required: true },
+	name: { type: Types.Name, required: true, index: true }
+	, chineseName: { type: Types.Text }
+	, team: { type: Types.Select, options: 'BIG, English, both' }
+	, image: { type: Types.CloudinaryImage }
+	, bio: { type: Types.Textarea }
+	, status: { type:Types.Select, options: 'active, alumni' }
+	, email: { type: Types.Email, initial: true, required: true, unique: true, index: true }
+	, password: { type: Types.Password, initial: true, required: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
 });
